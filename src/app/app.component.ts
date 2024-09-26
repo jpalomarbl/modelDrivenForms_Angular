1679ec18b31ceb02c7041e2af889fc36e195a1fe
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2024-09-26 13:48:08 +0100

    Creado componente Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './Components/login/login.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, LoginComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'modelDrivenForms_Angular';
