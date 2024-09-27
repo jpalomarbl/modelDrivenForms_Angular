5bb3801e6539fa83cc56ee0f4ba4bfe8e547f170
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2024-09-27 12:24:25 +0100

    Finalizado Login

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { UserDTO } from '../../Models/user.dto';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  user: UserDTO = new UserDTO('', '');
 
+  constructor() {}
+
+  checkLogin(): void {
+    console.log(
+      'User email --> ' + 
+        this.user.email +
+      ', User password --> ' +
+        this.user.password
+    )
+  }
 }
