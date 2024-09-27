a557afffb842a8b44fe6a29b66625061b0146253
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2024-09-27 12:38:30 +0100

    Added sign in route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './Components/login/login.component';
+import { SignInComponent } from './Components/sign-in/sign-in.component';
 
 export const routes: Routes = [
     {
         path:'',
         component: LoginComponent
+    },
+    {
+        path:'sign-in',
+        component: SignInComponent
     }
 ];
